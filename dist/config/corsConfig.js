516fef29e9836bdd5e7e716cecf349a92acfa7aa
Benjamin Likita <benjaminlikita3@gmail.com>
2025-04-16 20:20:10 +0100

    Delete dist directory

--- dist/config/corsConfig.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.corsOptions = void 0;
-exports.corsOptions = {
-    origin: process.env.CORS_ORIGIN,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-};
--- dist/routes/webhooks.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const webhooksController_js_1 = require("../controller/webhooksController.js");
-const router = (0, express_1.Router)();
-router.post("/clerk", webhooksController_js_1.clerkController);
-exports.default = router;
